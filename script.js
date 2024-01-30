dfe1ea0c28ec3e927462bcf987f38ffb6fe5cf3e
OKuzminova <yagodka029@yandex.ru>
2024-01-30 20:37:13 +0300

    Module 10 - JS
    Рефакторинг закрытия формы, настройка работы кнопок для слайдера

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 var basket = document.querySelector(".nav-link");
+var basketFooter = document.querySelector(".button__footer");
 var modal = document.querySelector(".modal");
 var fullName = modal.querySelector("[name=fullName]");
 
-basket.addEventListener("click", function (evt) {
-  evt.preventDefault();
+
+// Модальное окно – открытие
+function deleteClass (event) {
+  event.preventDefault();
   console.log("По мне кликнули");
   modal.classList.remove("form-hidden");
   fullName.focus();
-});
+};
 
-var close = document.querySelector(".modal-close");
-close.addEventListener("click", function () {
+basket.addEventListener("click", deleteClass); //Открытие формы в шапке
+basketFooter.addEventListener("click", deleteClass); //Открытие формы в футере
+
+
+var closeModal = document.querySelector(".modal-close");
+var body = document.querySelector("main");
+
+// Модальное окно – закрытие
+function addClass () {
   modal.classList.add("form-hidden");
   console.log("Меня закрыли");
+};
+
+closeModal.addEventListener("click", addClass); //Закртытие по кнопке
+// body.addEventListener("click", addClass); //Закрытие при нажатии окна в любом месте
+
+
+//Слайдер
+var blockSlider = document.querySelector(".slider-controls"); //Блок со всеми кнопками
+console.log(blockSlider);
+var btnSlider = blockSlider.querySelectorAll("button"); //Массив кнопок слайдера
+console.log(btnSlider);
+const arrBtn = [];
+
+blockSlider.addEventListener("click", function(active) {
+  const target = active.target
+  Array.from(btnSlider).forEach(item => {
+    item.classList.remove("current");
+    console.log("remove");
+  });
+  target.classList.add("current");
+  console.log("add");
 });
-
-var body = document.querySelector("main");
-body.addEventListener("click", function () {
-  modal.classList.add("form-hidden");
-  console.log("Клик по окну");
-});
-
-
-
